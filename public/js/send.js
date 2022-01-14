472a66330ff4361cd6a930bbfb06eb83f7da740a
Fabio <fabiosds17@gmail.com>
2022-01-14 18:52:38 -0300

    backend restaurado

--- public/js/send.js
+++ public/js/send.js
@@ -1,21 +1,5 @@
-function submit(){
-    let number = document.getElementById("input").value
-    return new Send(number)
-    
-}
+const num = document.getElementById('input').value
+const str = toString(num, 8)
+const rep = str.replace(/[\u0300-\u036f]/g, "")
 
-class Send {
-    constructor(number){
-        this.number = number
-        this.send()
-    }
-
-    send(){
-        this.validator()
-    }
-
-    validator(){
-        this.number = parseInt(this.number, 10)
-        return window.alert(this.number)
-    }
-}
+console.log(rep);
