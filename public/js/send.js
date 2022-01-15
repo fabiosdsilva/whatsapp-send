e716e72b1f92afd3898dbe018e7963c7253152b2
Fabio <fabiosds17@gmail.com>
2022-01-15 15:34:04 -0300

    estou ainda na classe Send

--- public/js/send.js
+++ public/js/send.js
@@ -5,19 +5,25 @@ function submit() {
 class Send {
   constructor() {
     this.numero = document.getElementById('input').value;
+    this.Validar();
   }
 
   Validar() {
-    if (this.numero.length > 11) {
-      throw this.erro();
+    if (this.numero.length === 11) {
+      return this.sucesso();
     }
+    return this.erro();
+  }
+
+  Enviar() {
+    console.log('faibo');
   }
 
   sucesso() {
-
+    return document.getElementById('alert_sucess').style.display = '', document.getElementById('alert_sucess').innerHTML = "Tudo certo :)";
   }
 
-  erro(msg) {
-
+  erro() {
+    return document.getElementById('alert_danger').style.display = '', document.getElementById('alert_danger').innerHTML = "O número passado deve ter 11 digitos";
   }
 }
