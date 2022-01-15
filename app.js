66d593e94f615c87059d5c6aef4789138987bce1
Fabio <fabiosds17@gmail.com>
2022-01-15 18:32:26 -0300

    add cors

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ dotenv.config();
 import express from 'express';
 import routes from './controller/home';
 
+import cors from 'cors'
+
 class App {
   constructor() {
     this.app = express();
@@ -13,6 +15,7 @@ class App {
   }
 
   middleware() {
+    this.app.use(cors())
     this.app.use(express.static('public'));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.set('view engine', 'ejs');
