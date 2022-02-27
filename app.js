c5b8c10699c5d76fd40c0ad782362092dd425086
Fabio Silva <67711594+fabiosdsilva@users.noreply.github.com>
2022-02-27 05:58:10 -0300

    Removed cors()

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ dotenv.config();
 import express from 'express';
 import routes from './controller/home';
 
-import cors from 'cors'
-
 class App {
   constructor() {
     this.app = express();
@@ -15,7 +13,6 @@ class App {
   }
 
   middleware() {
-    this.app.use(cors())
     this.app.use(express.static('public'));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.set('view engine', 'ejs');
